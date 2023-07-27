f93dd8d08d83d2233ec89823a4f1480bebbd8610
adzumano <zkaragoyshin1@gmail.com>
2023-07-27 10:08:11 +0500

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,8 @@
 import { AppRouter } from 'app/providers/router';
-import { Suspense, useEffect } from 'react';
-import { KVStorage, LocalStorage } from 'shared/lib/storage';
+import { Suspense } from 'react';
 import { Header } from 'widgets/Header';
 
 export const App = () => {
-    const storage = new KVStorage(new LocalStorage());
-
-    useEffect(() => {
-        storage.set('key', 'value');
-        console.log(storage.get('key'));
-    }, []);
     return (
         <div className={'app'}>
             <Header />
