b7f1c05ad3716dc88a83dfa7b3a68dd2cfc29da5
ktulu92 <sludgekicker666@gmail.com>
2023-10-06 03:38:24 +0300

    webpack scss

---
+++ src/entities/cell/models/Cell.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import styles from "./ToolBar.modules.scss";
+
+
+
+export  function Cell (){
+
+    return <div 
+ 
+    >
+ячейка
+
+    </div>
+}
+
---
+++ src/entities/cell/types.ts
@@ -0,0 +1,12 @@
+export interface CellType {
+  id: number;
+    row:number;
+    column:string;
+    fontStyle:string;
+    fontSize:number;
+    fontColor:string;
+    contentPosition:string;
+    backgroundColor:string;
+
+  }
+  
